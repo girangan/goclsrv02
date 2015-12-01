7b5e1a00bb437e0048ab2aea771a958a99ebab52
Girish <girangan@cisco.com>
2015-12-01 15:08:22 -0500

    Initial commit

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "json"
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped Moooooo!</h1>\n")
+    
+    persons, err := runTestDB();
+    if err != nil {
+        http.Error(w, "Damn!. . .", 500)
+        return
+    }
+    w.Header.Set('Content-type', 'application/json');
+    err = json.NewEncoder(w).Encode(persons);
+    if err != nil {
+        http.Error(w, "Damn that was close!. . .", 500)
+    }
 }
 
 func main() {
