e88b4a7c3a4e859b014dbf916f0a8d4cd86539f0
Girish <girangan@cisco.com>
2015-12-02 01:45:18 -0500

    log error

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     persons, err := runTestDB()
     if err != nil {
+        fmt.Println("Error:", err)
         http.Error(w, "Damn!. . .", 500)
         return
     }
     w.Header().Set("Content-type", "application/json")
     err = json.NewEncoder(w).Encode(persons);
     if err != nil {
+        fmt.Println("Error:", err)
         http.Error(w, "Damn that was close!. . .", 500)
     }
 }
